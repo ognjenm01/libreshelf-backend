138c5a985f613570403c86ab97e36e44012d43ab
Ognjen <ognjenmilojevic2001@gmail.com>
2024-02-19 16:20:07 +0100

    feat: created project for tests

---
+++ LibreShelfTests/SanityTests.cs
@@ -0,0 +1,12 @@
+namespace LibreShelfTests
+{
+    public class SanityTests
+    {
+        //If this fails, all hell broke loose
+        [Fact]
+        public void GlobalSanityTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
